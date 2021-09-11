993f1dc4c8419e1421d13294e68ea2d5ae340315
Rizart Dokollari <r.dokollari@gmail.com>
2021-09-11 11:50:04 +0100

    Initialise project with infrastructure.

---
+++ infrastructure/lib/infrastructure-stack.ts
@@ -0,0 +1,26 @@
+import * as cdk from "@aws-cdk/core";
+import * as codecommit from "@aws-cdk/aws-codecommit";
+import * as amplify from "@aws-cdk/aws-amplify";
+
+export class  InfrastructureStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const repository = new codecommit.Repository(
+        this,
+        "NextJSSSRAWSAmplify",
+        {
+          repositoryName: "next-jsr-ssr-aws-amplify",
+          description:
+              "CodeCommit repository that will be used as the source repository for next.js showcase",
+        }
+    );
+
+    const amplifyApp = new amplify.App(this, "sample-react-app ", {
+      sourceCodeProvider: new amplify.CodeCommitSourceCodeProvider({
+        repository: repository,
+      }),
+    });
+    amplifyApp.addBranch("main");
+  }
+}
