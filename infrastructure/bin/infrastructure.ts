08e0d2a85566ce5a6836e1f179f7633f9fc97088
Rizart Dokollari <r.dokollari@gmail.com>
2021-09-11 18:37:14 +0100

    Change stack name.

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -4,7 +4,7 @@ import * as cdk from '@aws-cdk/core';
 import { InfrastructureStack } from '../lib/infrastructure-stack';
 
 const app = new cdk.App();
-new InfrastructureStack(app, 'InfrastructureStack', {
+new InfrastructureStack(app, 'next-js-with-ssr', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
